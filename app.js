e2472916cd70c5381b564d8dfa69115b9a6fb0ce
mdemo <mengdesen09@qq.com>
2013-12-10 16:12:56 +0800

    test async

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ var lineReader = require('line-reader');
 var fs = require('fs');
 var Bagpipe = require('bagpipe');
 var async = require('async');
-var test = function(data){
-    (function(url){
+var getUrlSpeed = function(url,callback){
         var basset = new Basset(url[1],{ repeatNum: 1 });
         basset.on('end', function (result) {
             if(result.results[0]){
+                callback(false,result);
                 console.log('append to result.txt')
                 fs.appendFile('result.txt', url[0]+" , "+result.results[0].values.onLoad+"\n", function (err) {
                     if (err) throw err;
                 });
             }
             else{
-                console.log(url[1]);
-                console.log("fail");
+                callback(true);
             }
         });
         basset.on('testStart', function (result) {
@@ -25,33 +24,16 @@ var test = function(data){
             console.log("stop")
         });
         basset.sniff();
-    })(data);
-}
-var test2 = function(data,callback){
-    callback(data);
-}
-var bagpipe = new Bagpipe(3);
-var q = async.queue(function(data,callback){
-   test2(data,callback); 
-},3);
-q.drain = function(){
-    console.log("all done");
 };
+
+var bagpipe = new Bagpipe(3);
 lineReader.eachLine('urls.txt', function(line, last) {
     var data = line.split("	");
-    /*
-    bagpipe.push(test2,data,function(err){
-        console.log(err);
-    }); 
-    q.push(data,function(err){
-        console.log(err);
-    });*/
-});
-
-for(var i=0;i<10;i++){
-    bagpipe.push(test2,i,function(data){
-        console.log(data);
+    bagpipe.push(test2,data,function(err,data){
+        if(!err){
+            console.log(data);
+        }
     });
-}
+});
 
 
